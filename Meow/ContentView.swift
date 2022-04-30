d9dc74d68c1d5a653d6a977bdb3f3e81d19269e2
Stan Stevens <sstevens404@gmail.com>
2022-04-30 18:44:08 -0400

    Initial Commit

---
+++ Meow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meow
+//
+//  Created by Stanford Stevens on 4/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meow/MeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeowApp.swift
+//  Meow
+//
+//  Created by Stanford Stevens on 4/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
